d0776a2fd5fd26fcccc1664f9261fa19fc90c280
Daniel Gustaw <gustaw.daniel@gmail.com>
2018-02-18 04:01:09 +0100

    Space and arrows do not cause page scrolling.

--- js/app.js
+++ js/app.js
@@ -142,6 +142,9 @@
                             },config.roundTime);
                         }
                 }
+                if([0, 32, 37, 38, 39, 40].indexOf(e.keyCode) > -1) {
+                    e.preventDefault();
+                }
             })
         },
         logResult: function () {
