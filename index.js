ca5d27f955b20b06eb2010bd31d57c15bd68159f
gustawdaniel <gustaw.daniel@gmail.com>
2018-03-18 23:53:13 +0100

    Scoker connection established

--- index.js
+++ index.js
@@ -1,8 +1,19 @@
-const app = require('express')();
+const express = require('express');
+const app = express();
 const http = require('http').Server(app);
+const io = require('socket.io')(http);
+
+app.use('/js', express.static(__dirname + '/js'));
+app.use('/css', express.static(__dirname + '/css'));
+app.use('/node_modules', express.static(__dirname + '/node_modules'));
 
 app.get('/', function(req, res){
-    res.send('<h1>Hello world</h1>');
+    // res.send('<h1>Hello world</h1>');
+    res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', function(socket){
+    console.log('a user connected');
 });
 
 http.listen(3000, function(){
