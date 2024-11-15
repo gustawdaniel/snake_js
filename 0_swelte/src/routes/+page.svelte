99a97ab477930dd0c68c701fbbdf281b96f19956
Daniel Gustaw <gustaw.daniel@gmail.com>
2024-11-15 22:22:08 +0400

    map added

--- 0_swelte/src/routes/+page.svelte
+++ 0_swelte/src/routes/+page.svelte
@@ -7,12 +7,22 @@
     };
 
     setInterval(move, 200);
+
+    let map = Array.from({ length: 20 }, () => Array.from({ length: 20 }, () => 0));
 </script>
 
 <div>
+    {#each map as row, y}
+        {#each row as cell, x}
+            <div class="map-cell" style="top: {y * 20}px; left: {x * 20}px;"></div>
+        {/each}
+    {/each}
+
     {#each snake as segment}
         <div class="snake-segment" style="top: {segment.y * 20}px; left: {segment.x * 20}px;"></div>
     {/each}
+
+
 </div>
 
 <style>
@@ -21,5 +31,13 @@
         width: 20px;
         height: 20px;
         background: green;
+    }
+
+    .map-cell {
+        position: absolute;
+        width: 20px;
+        height: 20px;
+        background: white;
+        border: 1px solid black;
     }
 </style>
