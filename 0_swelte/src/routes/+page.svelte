21a1bbfcb8962f07b8f415eb0145f82f14087c1e
Daniel Gustaw <gustaw.daniel@gmail.com>
2024-11-15 22:15:36 +0400

    added svelte snake

---
+++ 0_swelte/src/routes/+page.svelte
@@ -0,0 +1,25 @@
+<script>
+    let snake = [{ x: 5, y: 5 }];
+    let direction = { x: 1, y: 0 };
+
+    const move = () => {
+        snake = [{ x: snake[0].x + direction.x, y: snake[0].y + direction.y }, ...snake.slice(0, -1)];
+    };
+
+    setInterval(move, 200);
+</script>
+
+<div>
+    {#each snake as segment}
+        <div class="snake-segment" style="top: {segment.y * 20}px; left: {segment.x * 20}px;"></div>
+    {/each}
+</div>
+
+<style>
+    .snake-segment {
+        position: absolute;
+        width: 20px;
+        height: 20px;
+        background: green;
+    }
+</style>
